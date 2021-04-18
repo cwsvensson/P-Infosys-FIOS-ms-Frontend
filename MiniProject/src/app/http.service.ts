c89d83e2493ffde38956df3e72f9b1a6a3ec2903
jennhunter77 <jennhunter77@gmail.com>
2021-04-18 09:38:16 -0500

    updated get and post in service

--- MiniProject/src/app/http.service.ts
+++ MiniProject/src/app/http.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  private baseUrl = 'http://localhost:4200/subscribe';
+  private baseUrl = 'http://localhost:8083/subscribe';
 
   constructor(private http:HttpClient) { }
+
+  getSub(value:any): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${value.id}`);
+  }
+
+  postSub(value:any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/${value.id}`, value);
+  }
 }
