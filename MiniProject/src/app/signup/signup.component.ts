6eabfd9196963ee0092d765be7af667fd004d240
ClayFlores <ClayFlores@users.noreply.github.com>
2021-04-19 08:44:29 -0700

    added table
    table to display data from backend

--- MiniProject/src/app/signup/signup.component.ts
+++ MiniProject/src/app/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/http.service';
 
@@ -13,14 +14,26 @@ export class SignupComponent implements OnInit {
   constructor(private hs:HttpService) { }
 
   ngOnInit(): void {
+    
   }
+
+ public Data: any = {id: Number, name: String, cableSubscribed: String, internetSubscribed: String, phoneSubscribed: String};
 
   onSubmit(){
     console.log('in onSubmit');
     this.submitted = true;
     this.hs.getSub(Number(this.inputId)).subscribe(data => 
       {
-        document.getElementById("info").innerHTML = data.id + " " + data.name + " " + data.cableSubscribed + " " + data.internetSubscribed + " " + data.phoneSubscribed;
+        this.Data.id = data.id;
+        this.Data.name = data.name;
+        if(data.cableSubscribed) {this.Data.cableSubscribed = "Active"}
+        else{this.Data.cableSubscribed = "Inactive"}
+        if(data.internetSubscribed) {this.Data.internetSubscribed = "Active"}
+        else{this.Data.internetSubscribed = "Inactive"}
+        if(data.phoneSubscribed) {this.Data.phoneSubscribed = "Active"}
+        else{this.Data.phoneSubscribed = "Inactive"}
+        //document.getElementById("info").innerHTML = data.id + " " + data.name + " " + data.cableSubscribed + " " + data.internetSubscribed + " " + data.phoneSubscribed;
+        document.getElementById("dataTable").style.display = "flex";
         console.log(data);
       });
   }
